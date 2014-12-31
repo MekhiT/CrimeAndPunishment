09f6c7a8451fb76a0713294f6e20aaf5311caa19
Matthew Ha <matthewha123@gmail.com>
2014-12-31 12:46:43 -0500

    Stuff from 12/30-31

--- Assets/Scripts/CharControlMove.cs
+++ Assets/Scripts/CharControlMove.cs
@@ -6,21 +6,42 @@ public class CharControlMove : MonoBehaviour {
 
 	public float MoveSpeed;
 	public float RotationSpeed;
+	public float v;
+	public Vector3 movePos;
+	public Vector3 rotation;
+
 	CharacterController cc;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
 
 		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator> ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 movePos = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
-		transform.Rotate (new Vector3(0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+		v = Input.GetAxis ("Vertical");
+		movePos = v * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		animate (v);
+		//transform.Rotate (new Vector3(0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+		rotation = new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0);
+		transform.Rotate (rotation);
 		cc.Move (movePos*Time.deltaTime);
 		cc.SimpleMove (Physics.gravity);
+
 	
 	}
+
+	void animate (float ifInput){
+
+		bool walking = ifInput != 0f;
+
+		anim.SetBool ("IsWalking", walking);
+
+	}
+
+
 }
---
+++ Assets/Scripts/JacketFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JacketFollow : MonoBehaviour {
+
+	Vector3 JacketOffset;
+	public Vector3 targetPos;
+	public Transform playerTrans;
+	Vector3 playerRotation;
+	float jacketRotateSpeed;
+
+
+
+
+	GameObject player;
+	CharControlMove playerMove;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Rask");
+		playerMove = player.GetComponent<CharControlMove> ();
+
+		playerTrans = GameObject.FindWithTag ("Rask").transform;
+		JacketOffset = transform.position - playerTrans.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		targetPos = playerTrans.position + JacketOffset;
+		transform.position = targetPos;
+
+		transform.rotation = player.transform.rotation;
+
+
+
+	
+	}
+}
